74c21f976b6e23633d6f3b135bd1d412ce277757
Gagan <gagandeep280597@gmail.com>
2019-12-13 05:32:17 +0530

    Established communication between servers using axios

--- server/events/views.py
+++ server/events/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Event
+
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_events_list = Event.objects.order_by('-organised_on')[:5]
+    output = ', '.join([q.description for q in latest_events_list])
+    return HttpResponse(output)
+    # return HttpResponse(Event.objects.all())
+
+def detail(request, event_id):
+    return HttpResponse("You're looking at event %s." % event_id)
